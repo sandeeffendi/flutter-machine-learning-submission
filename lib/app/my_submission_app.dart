d5fd7a33af54c0c9247f38d5579a5a74631f2e75
sandeeffendi <sande.effendi@gmail.com>
2025-09-27 23:34:02 +0700

    fix: theme and route config on main

--- lib/app/my_submission_app.dart
+++ lib/app/my_submission_app.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:image_identification_submisison_app/app/app_router.dart';
+import 'package:image_identification_submisison_app/core/theme/app_textstyle.dart';
+import 'package:image_identification_submisison_app/core/theme/app_theme.dart';
 
 class MySubmissionApp extends StatelessWidget {
   const MySubmissionApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    // Initialise MaterialTheme and TextTheme
+    TextTheme textTheme = createTextTheme(context, 'monteserrat', 'poppins');
+    MaterialTheme theme = MaterialTheme(textTheme);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Machine Learning Submission App',
+      // Theme config
+      theme: theme.light(),
+      darkTheme: theme.light(),
+      themeMode: ThemeMode.system,
+      // Route config
+      initialRoute: AppRouter.splash,
+      onGenerateRoute: AppRouter.generateRouter,
+    );
   }
 }
